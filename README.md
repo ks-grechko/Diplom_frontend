0c0cedf8168cb95f16f758704c4481c19b1de0b3
Kseniia Grechko <grechko.ks@yandex.ru>
2020-04-05 12:45:49 +0300

    first commit: added webpack

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Diplom_frontend
+
+
+ v. 0.0.1
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader',
+                    'postcss-loader'
+                ]
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+            'file-loader?name=./images/[name].[ext]',
+            {
+                loader: 'image-webpack-loader',
+                options: {}
+            },
+        ]
+    },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ // 
+      filename: 'style.[contenthash].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+          preset: ['default'],
+      },
+      canPrint: true
+  }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
