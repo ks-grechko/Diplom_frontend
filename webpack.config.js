6d08ae0ebf7d11d6784440e032aa21230d411a0f
Kseniia Grechko <grechko.ks@yandex.ru>
2020-04-26 19:41:48 +0300

    Добавлена html верстка 3-х страниц + в отдельном файле прелоудер и результат поисковой выдачи, которые будут реализованы позже через js, карусель так же будет реализована через js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-  entry: { main: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+  entry: {
+    main: "./src/index.js",
+    about: "./src/about.js",
+    paper: "./src/paper.js",
   },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+  },
+
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-                use: [
-                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                    'css-loader',
-                    'postcss-loader'
-                ]
+        use: [
+          isDev ? "style-loader" :{loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+          }},
+          "css-loader", 
+          "postcss-loader",
+        ],
       },
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
@@ -37,26 +47,43 @@ module.exports = {
             },
         ]
     },
-    ]
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
+      }
+    ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ // 
-      filename: 'style.[contenthash].css',
+    new MiniCssExtractPlugin({
+      filename: "./[name].[contenthash].css",
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
+      cssProcessor: require("cssnano"),
       cssProcessorPluginOptions: {
-          preset: ['default'],
+        preset: ["default"],
       },
-      canPrint: true
-  }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: './src/index.html',
-      filename: 'index.html'
+      canPrint: true,
     }),
-    new WebpackMd5Hash()
-  ]
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/main.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/about.html",
+      filename: "about.html",
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/paper.html",
+      filename: "paper.html",
+    }),
+    new WebpackMd5Hash(),
+
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
 };
